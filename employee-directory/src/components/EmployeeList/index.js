151040eaa82199b3c296fbad5a60d2827524057e
Andrew Freire <andrew.m.freire@gmail.com>
2020-11-15 19:09:33 -0800

    Added Axios and API call for users

--- employee-directory/src/components/EmployeeList/index.js
+++ employee-directory/src/components/EmployeeList/index.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
+import API from "../../utils/API";
 
 export default class EmployeeList extends Component {
+
+    // When component mounts, get all the users from randomuser.me and assign to the state of EmployeeList
+    componentDidMount() {
+        API.getRandomUsers().then(users=>{
+            console.log("Users:", users);
+
+            // TODO: Set the state of this to have all users
+            this.setState({
+                users: users.data.results
+            })
+        })
+    }
 
     render() {
         return (
             <div>
-                
+
             </div>
         );
       }
