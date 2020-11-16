7319712ef5f01600a60a568af9bf6cd68548387d
Andrew Freire <andrew.m.freire@gmail.com>
2020-11-15 22:33:09 -0800

    Completed populating users in table when app loads

--- employee-directory/src/components/Employees/index.js
+++ employee-directory/src/components/Employees/index.js
@@ -1,15 +1,33 @@
 import React from "react";
 
-function Employees ({ users }){
+function Employees({ users }) {
 
-    if(users[0] !== undefined){
-        return(
-           <div>
-               Employees go here
-           </div> 
-        )
+    if (users[0] !== undefined && users[0].name !== undefined) {
+        return (
+            users.map(user => {
+                const name = user.name;
+                const profilePic = user.picture.medium;
+                const email = user.email;
+
+                return (
+                    <tbody>
+                        <tr>
+                            <td data-th="Image" className="align-middle">
+                                <img src={profilePic} className="img-responsive" />
+                            </td>
+                            <td data-th="Name" className="name-cell align-middle">
+                                {name.first} {name.last}
+                            </td>
+                            <td data-th="Email" className="align-middle">
+                                {email}
+                            </td>
+                        </tr>
+                    </tbody>
+                );
+            })
+        );
     } else {
-        return(
+        return (
             <div>
                 No employees found!
             </div>
